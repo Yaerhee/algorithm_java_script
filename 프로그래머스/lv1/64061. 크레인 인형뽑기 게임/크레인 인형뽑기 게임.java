69c604c9f297b1804940d620d9ad2f2d8606cda3
yaerhee <yaerhee@naver.com>
2022-06-27 15:07:39 +0900

    chore: Update solution with comments

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,28 +1,25 @@
-import java.util.List;
 import java.util.Stack;
-import java.util.Arrays;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        
-        Stack<Integer> picks = new Stack<Integer>();
-        
 
-        for (int i = 0; i < moves.length; i++) {
+        Stack<Integer> picks = new Stack<Integer>();  // Stack import하여 활용
+
+        for (int i = 0; i < moves.length; i++) { // moves의 길이만큼 순회
             for (int[] arr: board) {
                 int num = 0;
                 num = arr[moves[i]-1]; // moves의 각 숫자를 index로 변환
                 if (num != 0) {
-                    picks.push(num);
-                    arr[moves[i]-1] = 0;
-                    
+                    picks.push(num); // moves가 지정하는 arr의 index 값을 picks에 push
+                    arr[moves[i]-1] = 0; // pick 한 num은 0으로 주입(0은 선택되지 않음)
+
+                    // picks가 업데이트 될 때마다 size 만큼 스택 순회
                     for (int j = 0; j < picks.size(); j++) {
                         if (j > 0 && picks.get(j).equals(picks.get(j-1))) {
+                            picks.pop(); // picks의 두 값이 같을 경우 두 번 pop
                             picks.pop();
-                            picks.pop();
-                            answer += 2;
+                            answer += 2; // 두 번 pop 하는 만큼의 숫자 차감
                         }
                     }
                     break;
@@ -30,19 +27,6 @@ class Solution {
             }
         }
 
-//         List<Integer> pickList = Arrays.stream(pick).boxed().collect(Collectors.toList()); // pick 배열을 List 로 변환하여 remove 할 수 있도록 함
-
-//         for (int index: pick) {
-//             for (int i = 0; i+1 < pick.length; i++) {
-//                 if (i+1 < pickList.size()) {
-//                    if (pickList.get(i + 1).equals(pickList.get(i))) {
-//                     pickList.subList(i, i + 2).clear();
-//                     answer += 2;
-//                     } 
-//                 }
-//             }
-//         }
-        
         return answer;
     }
 }
